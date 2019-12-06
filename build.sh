d0cdb43dd59d0a197326e2f6d60077d9e5af0727
Egon Rath <egon.rath@gmail.com>
2019-12-06 07:20:17 +0100

    Updated to 0.82.24

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+
+RELEASE=$(cat com.github.joncampbell123.DOSBox-X.json | grep "branch" | awk -F '\"' '{print $4}')
+
+git submodule init
+git submodule update
+#git submodule update --remote --merge
+
 flatpak-builder --repo=repo --force-clean --install-deps-from=flathub build com.github.joncampbell123.DOSBox-X.json
+
 if [ $? -eq 0 ]; then
-	flatpak build-bundle repo dosbox-x.flatpak com.github.joncampbell123.DOSBox-X 0.82.21
+	flatpak build-bundle repo dosbox-x.flatpak com.github.joncampbell123.DOSBox-X ${RELEASE}
 fi
 
