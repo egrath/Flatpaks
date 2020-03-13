958ca6b3d58e8fb713efe943c06424341ee4d45b
Egon Rath <egon.rath@gmail.com>
2020-03-13 07:06:07 +0100

    Updated to 0.2.8 and addaed AArch64 build

--- build.sh
+++ build.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-BASE=com.github.trufanovnok.scantailor
+NAME=com.github.trufanovnok.scantailor
+ARCH=x86_64
 
-MANIFEST=${BASE}.json
-FLATPAK=${BASE}.flatpak
-RELEASE=$(cat ${MANIFEST} | grep "branch" | awk -F '\"' '{print $4}')
+if [ $# -ge 1 ]; then
+	ARCH=${1}
+	echo registering qemu-user-static
+	sudo podman run --rm --privileged multiarch/qemu-user-static --reset -p yes
+fi
 
-flatpak-builder --repo=repo --force-clean --install-deps-from=flathub build ${MANIFEST}
+echo Building ${NAME} for architecture: ${ARCH}
 
+RELEASE=$(cat ${NAME}.json | grep "branch" | awk -F '\"' '{print $4}')
+
+git submodule init
+git submodule update
+#git submodule update --remote --merge
+
+# build 
+COMMAND="flatpak-builder --arch=${ARCH} --repo=repo --force-clean --install-deps-from=flathub build_${ARCH} ${NAME}.json"
+
+echo running: ${COMMAND}
+${COMMAND}
+
+# pack everything up in a distributable file
 if [ $? -eq 0 ]; then
-	flatpak build-bundle repo ${FLATPAK} ${BASE} ${RELEASE}
+	COMMAND="flatpak build-bundle --arch=${ARCH} repo ${NAME}_${ARCH}.flatpak ${NAME} ${RELEASE}"
+	echo running: ${COMMAND}
+	${COMMAND}
 fi
 
