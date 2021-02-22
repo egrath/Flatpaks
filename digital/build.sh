70a2bef576103401e0fee984dbcca9de75303707
Egon Rath <egon.rath@gmail.com>
2021-02-22 10:25:33 +0100

    Updated Digital to 0.26

--- digital/build.sh
+++ digital/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 flatpak-builder build --force-clean --install-deps-from=flathub --repo=repo com.github.hneemann.digital.json
 if [ $? -eq 0 ]; then
-    flatpak build-bundle repo digital.flatpak com.github.hneemann.digital 0.25
+    flatpak build-bundle repo digital.flatpak com.github.hneemann.digital 0.26
 fi
 
